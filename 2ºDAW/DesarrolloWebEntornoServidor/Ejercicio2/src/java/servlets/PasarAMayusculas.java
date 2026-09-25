68f0d2c212dcfeb1da0ca60f081e861227b0f70f
Jose Luis <joseluismedina2019@gmail.com>
2026-09-25 11:23:06 +0200

    Se suben los proyectos actuales de Servidor

---
+++ 2ºDAW/DesarrolloWebEntornoServidor/Ejercicio2/src/java/servlets/PasarAMayusculas.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author vboxuser
+ */
+@WebServlet(name = "PasarAMayusculas", urlPatterns = {"/PasarAMayusculas"})
+public class PasarAMayusculas extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+           
+        String frase = request.getParameter("frase");
+        if( frase != null) {
+               frase.toUpperCase();
+        }
+        
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet PasarAMayusculas</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet PasarAMayusculas</h1>");
+            out.println("<h2>Pasa un texto para la frase original:</h2>");
+            out.println("<form action=\"PasarAMayusculas\" method=\"post\" >");
+            out.println("Frase:");
+            out.println("<input type=\"text\" name=\"frase\" maxlength=\"20\" />");
+            out.println("<input type=\"submit\" value=\"enviar\" />");
+            out.println("</form>");
+            if(frase != null){
+                out.println("<h2>"+ frase +"</h2>");
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
